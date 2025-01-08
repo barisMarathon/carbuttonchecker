889f5c095d2dba996e3b8a015558090477764226
Ahmet Barış Güzeltaş <149706875+barisMarathon@users.noreply.github.com>
2025-01-08 12:03:47 +0300

    Update addCurrentTime.mjs

--- functions/addCurrentTime/addCurrentTime.mjs
+++ functions/addCurrentTime/addCurrentTime.mjs
@@ -1,4 +1,3 @@
-// addCurrentTime.js
 const mysql = require('mysql2/promise');
 
 exports.handler = async function (event) {
@@ -17,12 +16,13 @@ exports.handler = async function (event) {
   });
 
   try {
-    await connection.execute("INSERT INTO car (datetime_column) VALUES (NOW())");
+    // Insert current time + 2 hours into the database
+    await connection.execute("INSERT INTO car (datetime_column) VALUES (DATE_ADD(NOW(), INTERVAL 2 HOUR))");
     await connection.end();
 
     return {
       statusCode: 200,
-      body: 'Current time added successfully!',
+      body: 'Current time (+2 hours) added successfully!',
     };
   } catch (error) {
     return {
